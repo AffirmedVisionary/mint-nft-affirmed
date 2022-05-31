00b1875e5c4d6e0e1f6946f4b26e7dca615f462c
Charlene Grant <68870508+AffirmedVisionary@users.noreply.github.com>
2022-05-31 07:33:17 +0000

    initial set up

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# mint-nft-affirmed
+# Basic Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
+
+```json
+{
+    "name": "Famous Affirmed Visionary Quotes",
+    "description": "Stop treating your souls purpose like a side chick",
+    "image": "https://res.cloudinary.com/charlenegrant/image/upload/v1618480796/70896700_745406345872454_3260945975841893469_n.jpg_vmsy8n.jpg"
+}
+```
---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+// We inherit the contract we imported. This means we'll have access
+// to the inherited contract's methods.
+contract MyEpicNFT is ERC721URIStorage {
+  // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  // We need to pass the name of our NFTs token and its symbol.
+  constructor() ERC721 ("SquareNFT", "SQUARE") { // nft standard ERC721 https://eips.ethereum.org/EIPS/eip-721
+        console.log("This is my first NFT contract. Watch me soar!");
+  }
+
+  // A function our user will hit to get their NFT.
+  function makeAnEpicNFT() public {
+     // Get the current tokenId, this starts at 0.
+    uint256 newItemId = _tokenIds.current(); // state variable that gives each nft there own UID
+
+     // Actually mint the NFT to the sender using msg.sender.
+    _safeMint(msg.sender, newItemId);
+
+    // Set the NFTs data.
+    _setTokenURI(newItemId, "blah");
+
+    // Increment the counter for when the next NFT is minted.
+    _tokenIds.increment();
+  }
+}
